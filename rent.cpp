02915dc512046b29773a622b80d29ab1fc013de6
gunxroswar <gxz.gunforce@gmail.com>
2022-02-19 17:20:50 +0700

    "Maybe complete borrowbook part"

--- rent.cpp
+++ rent.cpp
@@ -6,7 +6,8 @@
 #include<iomanip>
 using namespace std;
 
-int id;
+int aidee;
+vector<string> book,borrowed,a,b,c,d,e,f,g,h,i,j,codes ;//ตัวแปรสำคัญๆ
 
 string toUpperStr(string x){
     string y = x;
@@ -14,15 +15,106 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(string filename,vector<string> &book){
+void allbook(vector<string> &book){
     ifstream source;
-    source.open(filename);
+    source.open("All.txt");
     string text;
     while(getline(source,text)){
         book.push_back(text);
         
-    }
+    } 
 }
+void general(vector<string> &a){
+    ifstream source;
+    source.open("General.txt");
+    string text;
+    while(getline(source,text)){
+        a.push_back(text);
+        
+    } 
+}
+void science(vector<string> &b){
+    ifstream source;
+    source.open("Science.txt");
+    string text;
+    while(getline(source,text)){
+        b.push_back(text);
+        
+    } 
+}
+void math(vector<string> &c){
+    ifstream source;
+    source.open("Math.txt");
+    string text;
+    while(getline(source,text)){
+        c.push_back(text);
+        
+    } 
+}
+void novel(vector<string> &d){
+    ifstream source;
+    source.open("Novel.txt");
+    string text;
+    while(getline(source,text)){
+        d.push_back(text);
+        
+    } 
+}
+void documentary(vector<string> &e){
+    ifstream source;
+    source.open("Documentary.txt");
+    string text;
+    while(getline(source,text)){
+        e.push_back(text);
+        
+    } 
+}
+void politic(vector<string> &f){
+    ifstream source;
+    source.open("Politics.txt");
+    string text;
+    while(getline(source,text)){
+        f.push_back(text);
+        
+    } 
+}
+void language(vector<string> &g){
+    ifstream source;
+    source.open("Language.txt");
+    string text;
+    while(getline(source,text)){
+        g.push_back(text);
+        
+    } 
+}
+void psycho(vector<string> &h){
+    ifstream source;
+    source.open("Phychology.txt");
+    string text;
+    while(getline(source,text)){
+        h.push_back(text);
+        
+    } 
+}
+void manage(vector<string> &i){
+    ifstream source;
+    source.open("Management.txt");
+    string text;
+    while(getline(source,text)){
+        i.push_back(text);
+        
+    } 
+}
+void techno(vector<string> &j){
+    ifstream source;
+    source.open("Technology.txt");
+    string text;
+    while(getline(source,text)){
+        j.push_back(text);
+        
+    } 
+}
+
 
 void getAns(string &ans){
     cin >> ans;
@@ -30,34 +122,38 @@ void getAns(string &ans){
 }
 
 
-void borrowBook(vector<string> &book,vector<string> &borrowed){
+void borrowBook(vector<string> &book,vector<string> &borrowed){ //โค้ดยืมหนังสือ ไว้ใส่ในปิกาจู
     int N=book.size();
     bool k=false;
-    string key;
+    string key,i;
     cout << "Please enter book's name: ";
     getline(cin,key);
     for(int i=0;i<N;i++){
         if(toUpperStr(key) == book[i]){
             k =true;
             if(k == true){
-                cout << "---------------------------------\n";
-                cout << "This is your " << book[i] << " GL HF\n";
-                cout << "---------------------------------\n";
+                cout << "............................................................................\n";
+                cout << " This is your " << book[i] << " Good Luck Have Fun.\n";
+                cout << "............................................................................\n";
                 borrowed.push_back(book[i]);
+                
             }
         }
     }
     if(k==false){
-        cout << "---------------------------------\n";
-        cout << "Cannot found.\n";
-        cout << "---------------------------------\n";  
+        cout << "............................................................................\n";
+        cout << "                            Cannot found.\n";
+        cout << "............................................................................\n";
     }
 
 }   
 
-void pikachu(vector<string> &book,vector<string> &borrowed,int &id){
+
+void pikachu(vector<string> &book,vector<string> &borrowed,int &aidee){ //โค้ดตัวหลัก
     string ans;
     bool k=true;
+    cout << "Please enter your id: ";
+    cin >> aidee;
     cout << "Before we start do you know the name of book which you desired?(Yes/No): ";
     do{
         getAns(ans);
@@ -65,45 +161,101 @@ void pikachu(vector<string> &book,vector<string> &borrowed,int &id){
         if(ans=="YES"){
             do{
                 borrowBook(book,borrowed);
-                cout << "Do you want to borrow any?(Yes/No): ";    
+                cout << "Do you want to borrow any?(Key No to exit,Key any to continue): ";    
                 getAns(ans);
+                
                 cin.ignore();
                 if(ans=="NO"){
-                    cout << "---------------------------------\n";
+                    cout << "............................................................................\n";
                     cout << "This are "<< borrowed.size() << " books which you wanna borrowing\n";
                     for(int i=0;i<borrowed.size();i++){
-                        
-                        cout << borrowed[i] << "\n";
+                        cout << setw(39) << borrowed[i]  <<"\n";
                     }
-                    cout << "Please return those book in due dates.\n";
-                    cout <<  "Thank you for using us.^_^\n";
-                    cout << setw(20) <<"Adios\n";            
-                    cout << "---------------------------------\n";
+                    cout << "                   Please return those book in due dates.\n";
+                    cout << "                       Thank you for using us.^_^\n";
+                    cout << "                                 Adios\n";            
+                    cout << "............................................................................\n";
                     k=false;
                     break;
                 } 
             }while(true);
-            
+            break;
         }else if(ans=="NO"){
-            //เว้นไว้ใส่ฟังก์ชั่นหาหนังสือ
+            cout << "                   You must \"SEACH BOOKS\" before borrow.\n";
             break;
         }else{
-            cout << "---------------------------------\n";
-            cout << "Invalid command.\n";
-            cout << "---------------------------------\n";
+            cout << "............................................................................\n";
+            cout << "                           Invalid command.\n";
+            cout << "............................................................................\n";
             cout << "Before we start do you know the name of book which you desired?(Yes/No): ";
         }
-    }while(k);
+    }while(true);
 
+    
+}
+
+vector<string> getCodes(vector<string> &book,vector<string> &borrowed,vector<string> &a,vector<string> &b,vector<string> &c,vector<string> &d,vector<string> &e
+        ,vector<string> &f,vector<string> &g,vector<string> &h,vector<string> &i,vector<string> &j){//โค้ดรับค่ารหัสหนังสือ
+    string x;
+    vector<string> codes;
+    for(int y=0;y<borrowed.size();y++){
+        for(int z=0;z<100;z++){
+            if(borrowed[y]==a[z]){
+                x = "A-"+to_string((z+1));
+                codes.push_back(x);
+                
+            }else if(borrowed[y]==b[z]){
+                x = "B-"+to_string(z+1);
+                codes.push_back(x);
+                
+            }else if(borrowed[y]==c[z]){
+                x = "C-"+to_string(z+1);
+                codes.push_back(x);
+                
+            }else if(borrowed[y]==d[z]){
+                x = "D-"+to_string(z+1);
+                codes.push_back(x);
+                
+            }else if(borrowed[y]==e[z]){
+                x = "E-"+to_string(z+1);
+                codes.push_back(x);
+                
+            }else if(borrowed[y]==f[z]){
+                x = "F-"+to_string(z+1);
+                codes.push_back(x);
+                
+            }else if(borrowed[y]==g[z]){
+                x = "G-"+to_string(z+1);
+                codes.push_back(x);
+                
+            }else if(borrowed[y]==h[z]){
+                x = "H-"+to_string(z+1);
+                codes.push_back(x);
+                
+            }else if(borrowed[y]==i[z]){
+                x = "I-"+to_string(z+1);
+                codes.push_back(x);
+               
+            }else if(borrowed[y]==j[z]){
+                x = "J-"+to_string(z+1);
+                codes.push_back(x);
+              
+            }
+        }
+    }
+    return codes;
+}
+
+void pass(vector<string> &borrowed,vector<string> &codes,int id){//ส่งข้อมูลเข้าไฟล์pass.txt
     ofstream dest("pass.txt");
-    for(int i=0;i<borrowed.size();i++){
-        dest << id << ": " << borrowed[i] << "\n" ;
-    } 
+        for(int i=0;i<borrowed.size();i++){
+            dest << id << ": " << borrowed[i] << "," << codes[i] << "\n" ;
+        } 
     dest.close();
 }
 
-void showBorrowed(string butterfly){
-    ifstream source(butterfly);
+void showBorrowed(){
+    ifstream source("pass.txt");
     string text;
     while(getline(source,text)){
         cout << text << "\n";
@@ -114,20 +266,33 @@ void showBorrowed(string butterfly){
 
 int main()
 {
-    int id;
-    cout << "plz enter your id: ";
-    cin >> id;
-    string filename = "All.txt";
-    string butterfly = "pass.txt";
-    vector<string> book,borrowed ;//ตัวแปรหนังสือกับหนังสือที่ถูกยืม
-    importDataFromFile(filename,book);
-    pikachu(book,borrowed,id);
 
-    string o;
+    
+    
+    allbook(book);
+    general(a);
+    science(b);
+    math(c);
+    novel(d);
+    documentary(e);
+    politic(f);
+    language(g);
+    psycho(h);
+    manage(i);
+    techno(j);
+    
+    pikachu(book,borrowed,aidee);
+   
+    codes = getCodes(book,borrowed,a,b,c,d,e,f,g,h,i,j);
+
+ 
+    pass(borrowed,codes,aidee);
+  
+    string o; //ใช้ฟังชั่นเรียกดูในไฟล์หนังสือที่ถูกยืมไป
     cout << "enter key: ";
     cin >> o;
-    if(o == "Yes"){
-        showBorrowed(butterfly);
+    if(o == "yes"){
+        showBorrowed();
     }
     
     return 0;
